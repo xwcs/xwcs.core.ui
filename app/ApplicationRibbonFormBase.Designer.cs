c5ba69a588bb79c9d9030ae27182fe985fb53f9b
Ladislav Sopko <lsopko@3di.it>
2016-06-24 12:51:52 +0200

    dev in progress

---
+++ app/ApplicationRibbonFormBase.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace xwcs.core.ui.app
+{
+	partial class ApplicationRibbonFormBase
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ApplicationRibbonFormBase));
+			DevExpress.Utils.Animation.PushTransition pushTransition1 = new DevExpress.Utils.Animation.PushTransition();
+			this.ribbonControl = new DevExpress.XtraBars.Ribbon.RibbonControl();
+			this.barButtonItem_FileSave = new DevExpress.XtraBars.BarButtonItem();
+			this.barButtonItem_FileSaveAll = new DevExpress.XtraBars.BarButtonItem();
+			this.barButtonItem_WS_New = new DevExpress.XtraBars.BarButtonItem();
+			this.barButtonItem_WS_Change = new DevExpress.XtraBars.BarButtonItem();
+			this.barButtonItem_View_Props = new DevExpress.XtraBars.BarButtonItem();
+			this.barButtonItem_View_Status = new DevExpress.XtraBars.BarButtonItem();
+			this.barButtonItem_View_Home = new DevExpress.XtraBars.BarButtonItem();
+			this.barButtonItem_Tools_Options = new DevExpress.XtraBars.BarButtonItem();
+			this.ribbonPageHome = new DevExpress.XtraBars.Ribbon.RibbonPage();
+			this.ribbonPageGroupHomeWorkspace = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+			this.ribbonPageGroupHomeView = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+			this.ribbonPageGroupHomeTools = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+			this.ribbonPagePlugins = new DevExpress.XtraBars.Ribbon.RibbonPage();
+			this.ribbonPageGroupHomeFile = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+			this.ribbonPageGroupPlgs_generic = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+			this.ribbonStatusBar = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+			this.documentManager = new DevExpress.XtraBars.Docking2010.DocumentManager(this.components);
+			this.tabbedView = new DevExpress.XtraBars.Docking2010.Views.Tabbed.TabbedView(this.components);
+			this.dockManager = new DevExpress.XtraBars.Docking.DockManager(this.components);
+			this.workspaceManager = new DevExpress.Utils.WorkspaceManager();
+			this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+			((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.documentManager)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.tabbedView)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dockManager)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// ribbonControl
+			// 
+			this.ribbonControl.ExpandCollapseItem.Id = 0;
+			this.ribbonControl.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl.ExpandCollapseItem,
+            this.barButtonItem_FileSave,
+            this.barButtonItem_FileSaveAll,
+            this.barButtonItem_WS_New,
+            this.barButtonItem_WS_Change,
+            this.barButtonItem_View_Props,
+            this.barButtonItem_View_Status,
+            this.barButtonItem_View_Home,
+            this.barButtonItem_Tools_Options});
+			resources.ApplyResources(this.ribbonControl, "ribbonControl");
+			this.ribbonControl.MaxItemId = 12;
+			this.ribbonControl.Name = "ribbonControl";
+			this.ribbonControl.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPageHome,
+            this.ribbonPagePlugins});
+			this.ribbonControl.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonControlStyle.Office2013;
+			this.ribbonControl.StatusBar = this.ribbonStatusBar;
+			// 
+			// barButtonItem_FileSave
+			// 
+			this.barButtonItem_FileSave.AllowGlyphSkinning = DevExpress.Utils.DefaultBoolean.False;
+			resources.ApplyResources(this.barButtonItem_FileSave, "barButtonItem_FileSave");
+			this.barButtonItem_FileSave.Glyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem_FileSave.Glyph")));
+			this.barButtonItem_FileSave.GlyphDisabled = ((System.Drawing.Image)(resources.GetObject("barButtonItem_FileSave.GlyphDisabled")));
+			this.barButtonItem_FileSave.Id = 1;
+			this.barButtonItem_FileSave.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem_FileSave.LargeGlyph")));
+			this.barButtonItem_FileSave.LargeGlyphDisabled = ((System.Drawing.Image)(resources.GetObject("barButtonItem_FileSave.LargeGlyphDisabled")));
+			this.barButtonItem_FileSave.Name = "barButtonItem_FileSave";
+			this.barButtonItem_FileSave.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonSave_ItemClick);
+			// 
+			// barButtonItem_FileSaveAll
+			// 
+			resources.ApplyResources(this.barButtonItem_FileSaveAll, "barButtonItem_FileSaveAll");
+			this.barButtonItem_FileSaveAll.Glyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem_FileSaveAll.Glyph")));
+			this.barButtonItem_FileSaveAll.Id = 3;
+			this.barButtonItem_FileSaveAll.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem_FileSaveAll.LargeGlyph")));
+			this.barButtonItem_FileSaveAll.LargeGlyphDisabled = ((System.Drawing.Image)(resources.GetObject("barButtonItem_FileSaveAll.LargeGlyphDisabled")));
+			this.barButtonItem_FileSaveAll.Name = "barButtonItem_FileSaveAll";
+			this.barButtonItem_FileSaveAll.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButton_SaveAll_ItemClick);
+			// 
+			// barButtonItem_WS_New
+			// 
+			resources.ApplyResources(this.barButtonItem_WS_New, "barButtonItem_WS_New");
+			this.barButtonItem_WS_New.Glyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem_WS_New.Glyph")));
+			this.barButtonItem_WS_New.Id = 4;
+			this.barButtonItem_WS_New.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem_WS_New.LargeGlyph")));
+			this.barButtonItem_WS_New.Name = "barButtonItem_WS_New";
+			this.barButtonItem_WS_New.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButton_Createnew_ItemClick);
+			// 
+			// barButtonItem_WS_Change
+			// 
+			resources.ApplyResources(this.barButtonItem_WS_Change, "barButtonItem_WS_Change");
+			this.barButtonItem_WS_Change.Glyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem_WS_Change.Glyph")));
+			this.barButtonItem_WS_Change.Id = 5;
+			this.barButtonItem_WS_Change.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem_WS_Change.LargeGlyph")));
+			this.barButtonItem_WS_Change.Name = "barButtonItem_WS_Change";
+			this.barButtonItem_WS_Change.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButton_Change_ItemClick);
+			// 
+			// barButtonItem_View_Props
+			// 
+			resources.ApplyResources(this.barButtonItem_View_Props, "barButtonItem_View_Props");
+			this.barButtonItem_View_Props.Glyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem_View_Props.Glyph")));
+			this.barButtonItem_View_Props.Id = 6;
+			this.barButtonItem_View_Props.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem_View_Props.LargeGlyph")));
+			this.barButtonItem_View_Props.Name = "barButtonItem_View_Props";
+			// 
+			// barButtonItem_View_Status
+			// 
+			resources.ApplyResources(this.barButtonItem_View_Status, "barButtonItem_View_Status");
+			this.barButtonItem_View_Status.Glyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem_View_Status.Glyph")));
+			this.barButtonItem_View_Status.Id = 7;
+			this.barButtonItem_View_Status.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem_View_Status.LargeGlyph")));
+			this.barButtonItem_View_Status.Name = "barButtonItem_View_Status";
+			// 
+			// barButtonItem_View_Home
+			// 
+			resources.ApplyResources(this.barButtonItem_View_Home, "barButtonItem_View_Home");
+			this.barButtonItem_View_Home.Glyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem_View_Home.Glyph")));
+			this.barButtonItem_View_Home.Id = 8;
+			this.barButtonItem_View_Home.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem_View_Home.LargeGlyph")));
+			this.barButtonItem_View_Home.Name = "barButtonItem_View_Home";
+			// 
+			// barButtonItem_Tools_Options
+			// 
+			resources.ApplyResources(this.barButtonItem_Tools_Options, "barButtonItem_Tools_Options");
+			this.barButtonItem_Tools_Options.Glyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem_Tools_Options.Glyph")));
+			this.barButtonItem_Tools_Options.Id = 9;
+			this.barButtonItem_Tools_Options.LargeGlyph = ((System.Drawing.Image)(resources.GetObject("barButtonItem_Tools_Options.LargeGlyph")));
+			this.barButtonItem_Tools_Options.Name = "barButtonItem_Tools_Options";
+			// 
+			// ribbonPageHome
+			// 
+			this.ribbonPageHome.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroupHomeWorkspace,
+            this.ribbonPageGroupHomeView,
+            this.ribbonPageGroupHomeTools});
+			this.ribbonPageHome.Image = ((System.Drawing.Image)(resources.GetObject("ribbonPageHome.Image")));
+			this.ribbonPageHome.Name = "ribbonPageHome";
+			resources.ApplyResources(this.ribbonPageHome, "ribbonPageHome");
+			// 
+			// ribbonPageGroupHomeWorkspace
+			// 
+			this.ribbonPageGroupHomeWorkspace.ItemLinks.Add(this.barButtonItem_WS_New);
+			this.ribbonPageGroupHomeWorkspace.ItemLinks.Add(this.barButtonItem_WS_Change);
+			this.ribbonPageGroupHomeWorkspace.Name = "ribbonPageGroupHomeWorkspace";
+			resources.ApplyResources(this.ribbonPageGroupHomeWorkspace, "ribbonPageGroupHomeWorkspace");
+			// 
+			// ribbonPageGroupHomeView
+			// 
+			this.ribbonPageGroupHomeView.ItemLinks.Add(this.barButtonItem_View_Home);
+			this.ribbonPageGroupHomeView.ItemLinks.Add(this.barButtonItem_View_Props);
+			this.ribbonPageGroupHomeView.ItemLinks.Add(this.barButtonItem_View_Status);
+			this.ribbonPageGroupHomeView.Name = "ribbonPageGroupHomeView";
+			resources.ApplyResources(this.ribbonPageGroupHomeView, "ribbonPageGroupHomeView");
+			// 
+			// ribbonPageGroupHomeTools
+			// 
+			this.ribbonPageGroupHomeTools.ItemLinks.Add(this.barButtonItem_Tools_Options);
+			this.ribbonPageGroupHomeTools.Name = "ribbonPageGroupHomeTools";
+			resources.ApplyResources(this.ribbonPageGroupHomeTools, "ribbonPageGroupHomeTools");
+			// 
+			// ribbonPagePlugins
+			// 
+			this.ribbonPagePlugins.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroupHomeFile,
+            this.ribbonPageGroupPlgs_generic});
+			this.ribbonPagePlugins.Image = ((System.Drawing.Image)(resources.GetObject("ribbonPagePlugins.Image")));
+			this.ribbonPagePlugins.Name = "ribbonPagePlugins";
+			resources.ApplyResources(this.ribbonPagePlugins, "ribbonPagePlugins");
+			// 
+			// ribbonPageGroupHomeFile
+			// 
+			this.ribbonPageGroupHomeFile.ItemLinks.Add(this.barButtonItem_FileSave);
+			this.ribbonPageGroupHomeFile.ItemLinks.Add(this.barButtonItem_FileSaveAll);
+			this.ribbonPageGroupHomeFile.Name = "ribbonPageGroupHomeFile";
+			resources.ApplyResources(this.ribbonPageGroupHomeFile, "ribbonPageGroupHomeFile");
+			// 
+			// ribbonPageGroupPlgs_generic
+			// 
+			this.ribbonPageGroupPlgs_generic.Name = "ribbonPageGroupPlgs_generic";
+			resources.ApplyResources(this.ribbonPageGroupPlgs_generic, "ribbonPageGroupPlgs_generic");
+			// 
+			// ribbonStatusBar
+			// 
+			resources.ApplyResources(this.ribbonStatusBar, "ribbonStatusBar");
+			this.ribbonStatusBar.Name = "ribbonStatusBar";
+			this.ribbonStatusBar.Ribbon = this.ribbonControl;
+			// 
+			// documentManager
+			// 
+			this.documentManager.ContainerControl = this;
+			this.documentManager.MenuManager = this.ribbonControl;
+			this.documentManager.View = this.tabbedView;
+			this.documentManager.ViewCollection.AddRange(new DevExpress.XtraBars.Docking2010.Views.BaseView[] {
+            this.tabbedView});
+			// 
+			// dockManager
+			// 
+			this.dockManager.Form = this;
+			this.dockManager.TopZIndexControls.AddRange(new string[] {
+            "DevExpress.XtraBars.BarDockControl",
+            "DevExpress.XtraBars.StandaloneBarDockControl",
+            "System.Windows.Forms.StatusBar",
+            "System.Windows.Forms.MenuStrip",
+            "System.Windows.Forms.StatusStrip",
+            "DevExpress.XtraBars.Ribbon.RibbonStatusBar",
+            "DevExpress.XtraBars.Ribbon.RibbonControl",
+            "DevExpress.XtraBars.Navigation.OfficeNavigationBar",
+            "DevExpress.XtraBars.Navigation.TileNavPane"});
+			this.dockManager.Load += new System.EventHandler(this.dockManager_Load);
+			// 
+			// workspaceManager
+			// 
+			this.workspaceManager.TargetControl = this;
+			this.workspaceManager.TransitionType = pushTransition1;
+			this.workspaceManager.AfterApplyWorkspace += new System.EventHandler(this.workspaceManager_AfterApplyWorkspace);
+			this.workspaceManager.WorkspaceSaved += new DevExpress.Utils.WorkspaceEventHandler(this.workspaceManager_WorkspaceSaved);
+			// 
+			// ApplicationRibbonFormBase
+			// 
+			this.AllowFormGlass = DevExpress.Utils.DefaultBoolean.True;
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.ribbonStatusBar);
+			this.Controls.Add(this.ribbonControl);
+			this.Name = "ApplicationRibbonFormBase";
+			this.Ribbon = this.ribbonControl;
+			this.StatusBar = this.ribbonStatusBar;
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ApplicationFormBase_FormClosing);
+			this.Shown += new System.EventHandler(this.ApplicationFormBase_Shown);
+			((System.ComponentModel.ISupportInitialize)(this.ribbonControl)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.documentManager)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.tabbedView)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dockManager)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl;
+		private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPageHome;
+		private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroupHomeFile;
+		private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar;
+		private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroupHomeWorkspace;
+		private DevExpress.XtraBars.BarButtonItem barButtonItem_FileSave;
+		private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroupHomeView;
+		private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroupHomeTools;
+		private DevExpress.XtraBars.BarButtonItem barButtonItem_FileSaveAll;
+		private DevExpress.XtraBars.BarButtonItem barButtonItem_WS_New;
+		private DevExpress.XtraBars.BarButtonItem barButtonItem_WS_Change;
+		private DevExpress.XtraBars.BarButtonItem barButtonItem_View_Props;
+		private DevExpress.XtraBars.BarButtonItem barButtonItem_View_Status;
+		private DevExpress.XtraBars.BarButtonItem barButtonItem_View_Home;
+		private DevExpress.XtraBars.BarButtonItem barButtonItem_Tools_Options;
+		private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPagePlugins;
+		private DevExpress.XtraBars.Docking2010.DocumentManager documentManager;
+		private DevExpress.XtraBars.Docking2010.Views.Tabbed.TabbedView tabbedView;
+		private DevExpress.XtraBars.Docking.DockManager dockManager;
+		private DevExpress.Utils.WorkspaceManager workspaceManager;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+		private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroupPlgs_generic;
+	}
+}
--- app/ApplicationRibbonFormBase.cs
+++ app/ApplicationRibbonFormBase.cs
@@ -8,14 +8,359 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraBars;
+using xwcs.core.evt;
+using xwcs.core.manager;
+using xwcs.core.plgs;
+using xwcs.core.user;
+using xwcs.core.ui.controls;
+using DevExpress.XtraBars.Docking2010.Views;
+using DevExpress.XtraBars.Docking;
+using DevExpress.XtraBars.Ribbon;
+using DevExpress.XtraEditors;
+using System.IO;
 
 namespace xwcs.core.ui.app
 {
-	public partial class ApplicationRibbonFormBase : DevExpress.XtraBars.Ribbon.RibbonForm
+	public partial class ApplicationRibbonFormBase : DevExpress.XtraBars.Ribbon.RibbonForm, IPluginHost
 	{
+		private SEventProxy _proxy;         //just local singleton instance copy
+		private SWidgetManager _widgetManager; //main instance, who do it first it make it, this is just local singleton instance copy
+		private SPluginsLoader _loader;
+		private IUser _user;
+		private DocumentManagerSupport _managerSupport;
+
+		public SEventProxy eventProxy
+		{
+			get { return _proxy; }
+		}
+
+		public IUser currentUser
+		{
+			get { return _user; }
+		}
+
+
 		public ApplicationRibbonFormBase()
 		{
 			InitializeComponent();
+
+			/*
+			 * Assembly locating
+			 */
+
+			if (!DesignMode)
+			{
+				documentManager.View.DocumentProperties.UseFormIconAsDocumentImage = true;
+				documentManager.View.UseDocumentSelector = DevExpress.Utils.DefaultBoolean.True;
+				tabbedView.FloatingDocumentContainer = FloatingDocumentContainer.DocumentsHost;
+
+				// Security context must be call first time in main app and 
+				// must have providers set
+				_user = SecurityContext.getInstance().CurrentUser;
+
+				_proxy = SEventProxy.getInstance();
+				_proxy.addEventHandler<AddToolBarRequestEvent>(EventType.AddToolBarRequestEvent, HandleAddToolbarRequestEvent);
+				_proxy.addEventHandler<OpenPanelRequestEvent>(EventType.OpenPanelRequestEvent, HandleOpenPanelRequestEvent);
+
+				//do it here before any other plugin will be loaded!!!!!
+				_widgetManager = SWidgetManager.getInstance();
+
+				//now we can load plugins
+				_loader = SPluginsLoader.getInstance();
+				_loader.LoadPlugins(this, ".");
+
+				controls.ViewBaseEventsHandler.AttachToView(documentManager.View);
+
+				_managerSupport = new DocumentManagerSupport(documentManager,
+																new DevExpress.XtraBars.BarItem[] { barButtonItem_FileSave },
+																new DevExpress.XtraBars.BarItem[] { barButtonItem_FileSaveAll });
+				ribbonControl.SelectedPage = ribbonPagePlugins;
+			}
+
+		}
+
+
+
+		private void HandleOpenPanelRequestEvent(object sender, OpenPanelRequestEvent e)
+		{
+			OpenPanelRequest ee = (OpenPanelRequest)e.Data;
+			core.controls.VisualControlInfo vci = ee.Vci;
+
+			// handle eventual multi opening
+			if (!vci.AllowMulti)
+			{
+				BaseDocument existingDocument = _managerSupport.getDocumentByVCI(vci);
+				if (existingDocument != null)
+				{
+					documentManager.View.Controller.Activate(existingDocument);
+					VisualControl existingVisualControl = (VisualControl)existingDocument.Control;
+					if ((existingVisualControl != null) && (ee.DataObject != null))
+					{
+						existingVisualControl.Start(core.controls.VisualControlStartingKind.ActivateOpened, ee.DataObject);
+					}
+					return;
+				}
+			}
+
+			VisualControl control = (VisualControl)vci.createInstance();
+			if (control != null)
+			{
+
+				if (vci.DockStyle == core.controls.ControlDockStyle.PLGT_document)
+				{
+					documentManager.BeginUpdate();
+					BaseDocument document = documentManager.View.AddDocument(control);
+					document.Caption = control.VisualControlInfo.Name;
+					document.ControlName = control.VisualControlInfo.Name;
+					documentManager.EndUpdate();
+					documentManager.View.Controller.Activate(document);
+				}
+				else if (vci.DockStyle == core.controls.ControlDockStyle.PLGT_status)
+				{
+					DockPanel pb = new DockPanel();
+
+					if (dockManager.Panels.Count == 0)
+					{
+						pb = dockManager.AddPanel(DockingStyle.Bottom);
+					}
+					else
+					if (dockManager.Panels.Count == 1)
+					{
+						pb = dockManager.AddPanel(DockingStyle.Bottom);
+						DockPanel panelX = dockManager.Panels[0] as DockPanel;
+						pb.DockAsTab(panelX);
+					}
+					else
+					if (dockManager.Panels.Count > 1)
+					{
+						pb = dockManager.AddPanel(DockingStyle.Bottom);
+						DockPanel container = dockManager.Panels[0].ParentPanel as DockPanel;
+						if (container != null) pb.DockAsTab(container);
+					}
+
+					pb.ClosedPanel += (senderX, eX) =>
+					{
+						dockManager.RemovePanel(pb);
+					};
+
+
+					control.Dock = DockStyle.Fill;
+					pb.ControlContainer.Controls.Add(control);
+					pb.ID = vci.GUID;
+					pb.Name = vci.GUID + "_" + control.Name;
+					pb.Text = vci.Name;
+				}
+				else
+				{
+					DockPanel dockPanel1 = dockManager.AddPanel(DockingStyle.Top);
+					dockPanel1.ID = ee.Vci.GUID;
+					dockPanel1.Text = control.VisualControlInfo.Name;
+					dockPanel1.Height = 400;
+					dockPanel1.FloatSize = new Size(500, 400);
+
+					control.Dock = DockStyle.Fill;
+					dockPanel1.ControlContainer.Controls.Add(control);
+				}
+
+				//start control
+				control.Start(core.controls.VisualControlStartingKind.StartingNew, ee.DataObject);
+			}
+		}
+
+		private void addItemToRibonGroup(RibbonPageGroup sourceItem, DevExpress.XtraBars.BarItem newItem)
+		{
+			IEnumerable<BarItemLink> l = sourceItem.ItemLinks.Where(i => i.Caption == newItem.Caption).ToList();
+			if(l.Count() > 0) {
+				sourceItem.ItemLinks.Remove(l.First());
+			}
+
+			int newID = sourceItem.ItemLinks.Count;
+			newItem.Id = newID;
+			sourceItem.ItemLinks.Add(newItem);
+		}
+
+		private void HandleAddToolbarRequestEvent(object sender, AddToolBarRequestEvent e)
+		{
+			AddToolBarRequest ee = (AddToolBarRequest)e.Data;
+			MenuAddRequest[] menu = ee.content;
+
+			foreach (MenuAddRequest mar in menu)
+			{
+				switch (mar.destination)
+				{
+					case MenuDestination.MENU_file_open:
+						{
+							addItemToRibonGroup(ribbonPageGroupPlgs_generic , mar.content);
+							break;
+						}
+					case MenuDestination.MENU_ViewOtherWindows:
+						{
+							addItemToRibonGroup(ribbonPageGroupPlgs_generic, mar.content);
+							break;
+						}
+					case MenuDestination.MENU_tool_bar:
+						{
+							addItemToRibonGroup(ribbonPageGroupPlgs_generic, mar.content);
+							break;
+						}
+				}
+			}
+		}
+
+		private void dockManager_Load(object sender, EventArgs e)
+		{
+			foreach (DockPanel panel in dockManager.Panels)
+			{
+				XtraUserControl control = _loader.getControlByGuid(panel.ID);
+				if (control != null)
+				{
+					control.Dock = DockStyle.Fill;
+					panel.ControlContainer.Controls.Add(control);
+
+					panel.ClosedPanel += (senderX, eX) =>
+					{
+						dockManager.RemovePanel(panel);
+					};
+				}
+			}
+		}
+
+		private void workspaceManager_WorkspaceSaved(object sender, DevExpress.Utils.WorkspaceEventArgs args)
+		{
+			_managerSupport.SaveState();
+		}
+
+		private void workspaceManager_AfterApplyWorkspace(object sender, EventArgs e)
+		{
+			_managerSupport.LoadState();
+		}
+
+		private void loadWorkSpace()
+		{
+			if (DesignMode) return;
+
+			Stream reader = null;
+			try
+			{
+				reader = SPersistenceManager.getInstance().GetReader("DefaultWorkspace");
+				workspaceManager.LoadWorkspace("DefaultWorkspace", reader);
+				workspaceManager.CloseStreamOnWorkspaceLoading = DevExpress.Utils.DefaultBoolean.True;
+				workspaceManager.TransitionType = new DevExpress.Utils.Animation.ShapeTransition();
+				workspaceManager.TransitionType.Parameters.FrameCount = 1000;
+				workspaceManager.ApplyWorkspace("DefaultWorkspace");
+			}
+			catch (Exception ex)
+			{
+				SLogManager.getInstance().Error(ex.Message);
+			}
+			finally
+			{
+				if (reader != null) reader.Close();
+			}
+		}
+
+		private void saveWorkspace()
+		{
+			//https://www.devexpress.com/Support/Center/Example/Details/T190543
+			if (DesignMode) return;
+
+			Stream writer = null;
+			try
+			{
+				writer = SPersistenceManager.getInstance().GetWriter("DefaultWorkspace");
+				workspaceManager.CaptureWorkspace("DefaultWorkspace");
+				workspaceManager.CloseStreamOnWorkspaceSaving = DevExpress.Utils.DefaultBoolean.True;
+				workspaceManager.SaveWorkspace(workspaceManager.Workspaces[0].Name, writer, true);
+			}
+			catch (Exception ex)
+			{
+				SLogManager.getInstance().Error(ex.Message);
+			}
+			finally
+			{
+				if (writer != null) writer.Close();
+			}
+		}
+
+		protected void ApplicationFormBase_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			saveWorkspace();
+		}
+
+		protected void ApplicationFormBase_Shown(object sender, EventArgs e)
+		{
+			loadWorkSpace();
+			_proxy.fireEvent(new Event(this, EventType.WorkSpaceLoadedEvent, null));
+		}
+
+		private void barButton_Createnew_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+		{
+			//Create new workspace
+			if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+			{
+				try
+				{
+					documentManager.View.Controller.CloseAll();
+					workspaceManager.RemoveWorkspace("DefaultWorkspace");
+
+					SPersistenceManager.getInstance().CreateWorkSpace(folderBrowserDialog.SelectedPath);
+				}
+				catch (Exception ex)
+				{
+					SLogManager.getInstance().Error(ex.Message);
+				}
+			}
+		}
+
+		private void barButton_Change_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+		{
+			//Change workspace
+			if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+			{
+				try
+				{
+					SPersistenceManager.getInstance().SetWorkSpace(folderBrowserDialog.SelectedPath);
+
+					DockPanel[] p = new DockPanel[dockManager.Count];
+					int i = 0;
+					foreach (DockPanel dp in dockManager.Panels)
+					{
+						p[i++] = dp;
+					}
+					foreach (DockPanel dp in p)
+					{
+						dp.Close();
+					}
+
+					documentManager.View.Controller.CloseAll();
+					workspaceManager.RemoveWorkspace("DefaultWorkspace");
+					loadWorkSpace();
+				}
+				catch (IOException ex)
+				{
+					MessageBox.Show("This is not workspace's folder!");
+					SLogManager.getInstance().Info(ex.Message);
+				}
+				catch (Exception ex)
+				{
+					SLogManager.getInstance().Error(ex.Message);
+				}
+			}
+		}
+
+		private void barButton_SaveAll_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+		{
+			_managerSupport.SaveChangedControls();
+		}
+
+		private void barButtonSave_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+		{
+			_managerSupport.saveSelectedControl();
+		}
+
+		private void barButton_Exit_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+		{
+			Close();
 		}
 	}
 }
