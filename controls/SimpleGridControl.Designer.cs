7efb11f2a95019b87a993165d9235c8705b984c1
Pavol Pala <palap@ipoint.sk>
2017-04-24 11:10:05 +0200

    work in progress

---
+++ controls/SimpleGridControl.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace xwcs.core.ui.controls
+{
+	partial class SimpleGridControl
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SimpleGridControl));
+			this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+			this.gridControl = new DevExpress.XtraGrid.GridControl();
+			this.gridView = new DevExpress.XtraGrid.Views.Grid.GridView();
+			this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+			this.simpleButton_ADD = new DevExpress.XtraEditors.SimpleButton();
+			this.simpleButton_DELETE = new DevExpress.XtraEditors.SimpleButton();
+			this.tableLayoutPanel3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.gridControl)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.gridView)).BeginInit();
+			this.tableLayoutPanel4.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel3
+			// 
+			this.tableLayoutPanel3.ColumnCount = 2;
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+			this.tableLayoutPanel3.Controls.Add(this.gridControl, 0, 0);
+			this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel4, 1, 0);
+			this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+			this.tableLayoutPanel3.RowCount = 1;
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel3.Size = new System.Drawing.Size(660, 420);
+			this.tableLayoutPanel3.TabIndex = 3;
+			// 
+			// gridControl
+			// 
+			this.gridControl.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.gridControl.Location = new System.Drawing.Point(3, 3);
+			this.gridControl.MainView = this.gridView;
+			this.gridControl.Name = "gridControl";
+			this.gridControl.Size = new System.Drawing.Size(604, 414);
+			this.gridControl.TabIndex = 0;
+			this.gridControl.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+			this.gridView});
+			// 
+			// gridView
+			// 
+			this.gridView.GridControl = this.gridControl;
+			this.gridView.Name = "gridView";
+			this.gridView.OptionsBehavior.AllowSortAnimation = DevExpress.Utils.DefaultBoolean.False;
+			this.gridView.OptionsBehavior.Editable = false;
+			this.gridView.OptionsBehavior.ReadOnly = true;
+			this.gridView.OptionsCustomization.AllowFilter = false;
+			this.gridView.OptionsCustomization.AllowGroup = false;
+			this.gridView.OptionsCustomization.AllowSort = false;
+			this.gridView.OptionsSelection.EnableAppearanceFocusedCell = false;
+			this.gridView.OptionsView.ShowFilterPanelMode = DevExpress.XtraGrid.Views.Base.ShowFilterPanelMode.Never;
+			this.gridView.OptionsView.ShowGroupPanel = false;
+			// 
+			// tableLayoutPanel4
+			// 
+			this.tableLayoutPanel4.ColumnCount = 1;
+			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel4.Controls.Add(this.simpleButton_ADD, 0, 0);
+			this.tableLayoutPanel4.Controls.Add(this.simpleButton_DELETE, 0, 3);
+			this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel4.Location = new System.Drawing.Point(613, 3);
+			this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+			this.tableLayoutPanel4.RowCount = 4;
+			this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel4.Size = new System.Drawing.Size(44, 414);
+			this.tableLayoutPanel4.TabIndex = 1;
+			// 
+			// simpleButton_ADD
+			// 
+			this.simpleButton_ADD.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.simpleButton_ADD.Enabled = false;
+			this.simpleButton_ADD.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton_ADD.Image")));
+			this.simpleButton_ADD.Location = new System.Drawing.Point(3, 34);
+			this.simpleButton_ADD.Name = "simpleButton_ADD";
+			this.simpleButton_ADD.Size = new System.Drawing.Size(38, 34);
+			this.simpleButton_ADD.TabIndex = 2;
+			this.simpleButton_ADD.Text = "simpleButtonEdizione_INSERT";
+			// 
+			// simpleButton_DELETE
+			// 
+			this.simpleButton_DELETE.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.simpleButton_DELETE.Enabled = false;
+			this.simpleButton_DELETE.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton_DELETE.Image")));
+			this.simpleButton_DELETE.Location = new System.Drawing.Point(3, 344);
+			this.simpleButton_DELETE.Name = "simpleButton_DELETE";
+			this.simpleButton_DELETE.Size = new System.Drawing.Size(38, 34);
+			this.simpleButton_DELETE.TabIndex = 3;
+			this.simpleButton_DELETE.Text = "simpleButtonEdizione_DELETE";
+			// 
+			// SimpleGridControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.tableLayoutPanel3);
+			this.Name = "SimpleGridControl";
+			this.Size = new System.Drawing.Size(660, 420);
+			this.tableLayoutPanel3.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.gridControl)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.gridView)).EndInit();
+			this.tableLayoutPanel4.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+		public DevExpress.XtraGrid.GridControl gridControl;
+		public DevExpress.XtraGrid.Views.Grid.GridView gridView;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+		private DevExpress.XtraEditors.SimpleButton simpleButton_ADD;
+		private DevExpress.XtraEditors.SimpleButton simpleButton_DELETE;
+	}
+}
---
+++ controls/SimpleGridControl.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using System.Data.Entity;
+using xwcs.core.db;
+using System.Reflection;
+using DevExpress.XtraGrid.Columns;
+using xwcs.core.evt;
+using xwcs.core;
+
+
+namespace xwcs.core.ui.controls
+{
+	public partial class SimpleGridControl : DevExpress.XtraEditors.XtraUserControl
+	{
+		protected static xwcs.core.manager.ILogger _logger = xwcs.core.manager.SLogManager.getInstance().getClassLogger(typeof(SimpleGridControl));
+
+		protected DbContext _docDataContext;
+		protected xwcs.core.db.binding.GridBindingSource _bs;
+		private string _propertyName;
+		private EntityBase _container;
+		private Type _propertyType;
+
+
+		// methods
+		MethodInfo addRowMethod;
+		MethodInfo deleteRowMethod;
+
+		
+
+		public SimpleGridControl(xwcs.core.db.binding.IEditorsHost host, DbContext ctx, Type pt, string pn)
+		{
+			InitializeComponent();
+			_docDataContext = ctx;
+			_propertyType = pt;
+			_propertyName = pn;
+			_bs = new xwcs.core.db.binding.GridBindingSource(host);
+			_bs.Grid = gridControl;
+			simpleButton_ADD.Click += addRow;
+			simpleButton_DELETE.Click += deleteRow;
+
+			// make template function hook
+			addRowMethod = GetType().GetMethod("addRowGeneric", BindingFlags.NonPublic | BindingFlags.Instance).MakeGenericMethod(pt);
+			deleteRowMethod = GetType().GetMethod("deleteRowGeneric", BindingFlags.NonPublic | BindingFlags.Instance).MakeGenericMethod(pt);
+		}
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+
+			simpleButton_ADD.Click -= addRow;
+			simpleButton_DELETE.Click -= deleteRow;
+
+			base.Dispose(disposing);
+		}
+
+		public xwcs.core.db.binding.GridBindingSource BindingSource
+		{
+			get
+			{
+				return _bs;
+			}
+		}
+
+		public void readOnly(bool bOn)
+		{
+			gridView.OptionsSelection.EnableAppearanceFocusedCell = !bOn;
+			gridView.OptionsBehavior.Editable = !bOn;
+			gridView.OptionsBehavior.ReadOnly = bOn;
+			simpleButton_ADD.Enabled = !bOn;
+			simpleButton_DELETE.Enabled = !bOn;
+		}
+
+		public  virtual bool RefreshGrid(int movePosition)
+		{
+			int bookmark = _bs.Position;
+
+			if (_container != null)
+			{
+				GridColumn column = gridView.Columns.ColumnByFieldName("id");
+				//Check if exists column 'id' -> means there are right data. If not exists -> need clear old columns
+				if (column == null)
+					gridView.Columns.Clear();
+
+				SEventProxy.BlockModelEvents();				
+				_docDataContext.Entry(_container).Collection(_propertyName).Load();
+				SEventProxy.AllowModelEvents();
+
+				_bs.DataSource = _container.GetPropertyByName(_propertyName);
+
+				if (column == null) column = gridView.Columns.ColumnByFieldName("id");
+				if (column != null) column.SortOrder = DevExpress.Data.ColumnSortOrder.Ascending;
+				else _logger.Error("Can not find 'order' column");
+			}
+
+			if (bookmark != -1)
+			{
+				bookmark += movePosition;
+				_bs.Position = bookmark;
+			}
+			//Return false if empty
+			return (_bs.Count == 0)?false:true;
+		}
+
+		public bool RefreshGrid(EntityBase cnt, int movePosition)
+		{
+			_container = cnt;
+			return RefreshGrid(movePosition);
+		}
+
+
+		// EVENTS
+
+		protected void addRow(object sender, EventArgs e)
+		{
+			addRowMethod.Invoke(this, null);
+		}
+
+		protected void addRowGeneric<T>() where T : class
+		{
+			T curr = _bs.Current as T;
+			_bs.AddNew();
+			T newCurr = _bs.Current as T;
+
+			gridView.MoveLast();
+		}
+
+		protected void deleteRow(object sender, EventArgs e)
+		{
+			deleteRowMethod.Invoke(this, null);
+		}
+
+
+		protected void deleteRowGeneric<T>() where T : class
+		{
+			(_docDataContext.GetPropertyByName(_propertyName) as DbSet<T>).Remove(_bs.Current as T);
+
+			RefreshGrid(0);
+		}
+	}
+
+
+	public class BaseTableControl<T> : SimpleGridControl where T : EntityBase
+	{
+		private DbSet<T> _dbSet;
+
+		public BaseTableControl(xwcs.core.db.binding.IEditorsHost host, DbContext ctx, DbSet<T> ds) : base(host, ctx, typeof(T), "") {
+			_dbSet = ds;
+		}
+
+
+		public override bool RefreshGrid(int movePosition)
+		{
+			int bookmark = _bs.Position;
+
+
+			SEventProxy.BlockModelEvents();
+			_dbSet.Load();
+			_bs.DataSource = _dbSet.Local.ToBindingList();
+			SEventProxy.AllowModelEvents();
+
+			 
+
+			if (bookmark != -1)
+			{
+				bookmark += movePosition;
+				_bs.Position = bookmark;
+			}
+			//Return false if empty
+			return (_bs.Count == 0) ? false : true;
+		}
+	}
+}
