bea518e057998b4e6928478c2cc0b641712d59b5
Ladislav Sopko <lsopko@3di.it>
2017-07-06 16:02:01 +0200

    Rtf content added

---
+++ cnt/Rtf.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace xwcs.core.ui.cnt
+{
+    public partial class Rtf : UserControl, IContent
+    {
+        public Rtf()
+        {
+            InitializeComponent();
+        }
+
+        private string _RtfFilePath;
+        public string RtfFilePath
+        {
+            get {
+                return _RtfFilePath;       
+            }
+            set {
+                _RtfFilePath = value;
+            }
+        }
+
+        public void Load(string Path)
+        {
+            _RtfFilePath = Path;
+            richEditControl1.RtfText = File.ReadAllText(_RtfFilePath);
+
+        }
+    }
+}
