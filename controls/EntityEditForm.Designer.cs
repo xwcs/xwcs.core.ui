1671c75917c910e2a61b91e9b37925bdf53ece7a
egaf-pierof <piero.fiorentini@egaf.it>
2022-06-01 16:09:16 +0200

    Aggiornata edit form in modo da poterla ereditare per gestire gli eventuali loockup sui campi

--- controls/EntityEditForm.Designer.cs
+++ controls/EntityEditForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace xwcs.core.ui.controls
 {
-    partial class EntityEditForm<ModelType>
+    partial class EntityEditForm<ModelType, FormControlType>
 	{
         /// <summary>
         /// Required designer variable.
@@ -17,19 +17,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-			this.SuspendLayout();
-			// 
-			// EntityEditForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(410, 431);
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "EntityEditForm";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "EntityEditForm";
-			this.ResumeLayout(false);
+            this.SuspendLayout();
+            // 
+            // EntityEditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 426);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "EntityEditForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "EntityEditForm";
+            this.Load += new System.EventHandler(this.EntityEditForm_Load);
+            this.ResumeLayout(false);
 
         }
 
